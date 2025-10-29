eb7f624ccf9377835b5f6ce2979b23229d957fc8
TahmidSadat02 <githubsadat@gmail.com>
2025-10-30 01:40:13 +0600

    Add min_max.cpp

---
+++ lab_1/min_max.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Invalid size!" << endl;
+        return 0;
+    }
+
+    int arr[n];
+    cout << "Enter elements:\n";
+
+    cin >> arr[0];
+    int min = arr[0], max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        cin >> arr[i];
+
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+
+    cout << "Minimum = " << min << endl;
+    cout << "Maximum = " << max << endl;
+
+    return 0;
+}
